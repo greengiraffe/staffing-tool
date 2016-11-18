177fce33570b62f0ad24520d3c5b2e19026de4d3
Duc Anh Phi <ducanhphi@gmail.com>
2016-11-18 21:39:50 +0100

    [#00] refactor (skill.service.ts) -> simplify functions and remove redundant return statements

--- assets/app/skill/skill.service.ts
+++ assets/app/skill/skill.service.ts
@@ -9,8 +9,19 @@ import { Skill } from "./skill.model";
 export class SkillService {
     private skillCollection: Skill[] = [];
 
+    private extractData(res: Response) {
+      let body = res.json();
+      return body.data || {};
+    }
+
+    private handleError (error: Response | any) {
+      console.error(error);
+      return Observable.throw(error);
+    }
+
     constructor(private http: Http) {
     }
+
 
     addSkill(skill: Skill) {
         const body = JSON.stringify(skill);
@@ -19,16 +30,17 @@ export class SkillService {
             ? '?token=' + localStorage.getItem('token')
             : '';
         return this.http.post('http://localhost:3000/skill' + token, body, {headers: headers})
-            .map((response: Response) => {
-                const result = response.json();
-                const skill = new Skill(
-                    result.obj._id,
-                    result.obj.name,
-                    result.obj.rating);
-                this.skillCollection.push(skill);
-                return skill;
-            })
-            .catch((error: Response) => Observable.throw(error.json()));
+            // .map((response: Response) => {
+            //     const result = response.json();
+            //     const skill = new Skill(
+            //         result.obj._id,
+            //         result.obj.name);
+            //     this.skillCollection.push(skill);
+            //     return skill;
+            // })
+            // .catch((error: Response) => Observable.throw(error.json()));
+            .map(this.extractData)
+            .catch(this.handleError);
     }
 
     deleteSkill(skill: Skill) {
@@ -37,26 +49,29 @@ export class SkillService {
             ? '?token=' + localStorage.getItem('token')
             : '';
         return this.http.delete('http://localhost:3000/skill/' + skill.skillId + token)
-            .map((response: Response) => response.json())
-            .catch((error: Response) => Observable.throw(error.json()));
+            // .map((response: Response) => response.json())
+            // .catch((error: Response) => Observable.throw(error.json()));
+            .map(this.extractData)
+            .catch(this.handleError);
     }
 
-    getSkills() {
+    getSkills(): Observable<{}> {
         return this.http.get('http://localhost:3000/skill')
-            .map((response: Response) => {
-                const skills = response.json().obj;
-                let transformedSkills: Skill[] = [];
-                for (let skill of skills) {
-                    transformedSkills.push(new Skill(
-                        skill.obj._id,
-                        skill.obj.name,
-                        skill.obj.rating)
-                    );
-                }
-                this.skillCollection = transformedSkills;
-                return transformedSkills;
-            })
-            .catch((error: Response) => Observable.throw(error.json()));
+            // .map((response: Response) => {
+            //   console.log(response);
+            //     const skills = response.json();
+            //     let transformedSkills: Skill[] = [];
+            //     for (let skill of skills) {
+            //         transformedSkills.push(new Skill(
+            //             skill._id,
+            //             skill.name)
+            //         );
+            //     }
+            //     this.skillCollection = transformedSkills;
+            //     return transformedSkills;
+            // })
+            .map(this.extractData)
+            .catch(this.handleError);
     }
 
 }
