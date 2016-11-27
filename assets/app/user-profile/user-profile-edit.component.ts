f75ed057c4cd8d14fe3bdda9fd28d1914019c074
Duc Anh Phi <ducanhphi@gmail.com>
2016-11-27 17:39:04 +0100

    [#51] feat (user-profile-edit) -> add html structure and styling

--- assets/app/user-profile/user-profile-edit.component.ts
+++ assets/app/user-profile/user-profile-edit.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
-import { SkillSuggestionsComponent } from '../onboarding/skill/skill-suggestions.component'
+import { Component, OnInit } from '@angular/core';
+import { SkillSuggestionsComponent } from '../onboarding/skill/skill-suggestions.component';
+import { UserService } from '../_services/user.service';
+import { User } from '../_models/user.model';
 
 @Component({
   selector: 'app-user-profile-edit',
-  templateUrl: 'user-profile-edit.template.html'
+  templateUrl: 'user-profile-edit.template.html',
+  styleUrls: ['user-profile.style.scss']
 })
 
 export class UserProfileEditComponent {
 
+  user: User;
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit() {
+
+    this.userService.getUserById('5834d3782930d72869c78616')
+      .subscribe(
+        (user: User) => this.user = user,
+        error => console.log(error)
+      );
+  }
 }
