df3cb4791504b3d1e06866113215ca4057b36dbd
Valentin von Guttenberg <greengiraffe@posteo.de>
2016-11-29 15:28:12 +0100

    [#00] feat (seed) -> update seed fixture data

--- config/fixtures/development/SkillSeed.js
+++ config/fixtures/development/SkillSeed.js
@@ -10,5 +10,25 @@ module.exports = [
     {
         _id: '542c2b97bac0595474108b50',
         name: 'UI-Design'
+    },
+    {
+        _id: '542c2b97bac0595474108b51',
+        name: 'Paper Prototypes'
+    },
+    {
+        _id: '542c2b97bac0595474108b52',
+        name: 'Javascript'
+    },
+    {
+        _id: '542c2b97bac0595474108b53',
+        name: 'Sass'
+    },
+    {
+        _id: '542c2b97bac0595474108b54',
+        name: 'HTML'
+    },
+    {
+        _id: '542c2b97bac0595474108b55',
+        name: 'Healthcare'
     }
 ];
--- config/fixtures/development/UserSeed.js
+++ config/fixtures/development/UserSeed.js
@@ -2,6 +2,8 @@ module.exports = [
     {
         email: 'admin@ixds.com',
         password: '$2a$10$cIxLgLqYEVfOQl7VQfulL.zzmiTDn3Ugf1vUdu/OHiwQOyvQ.jz6a', // root
+        firstName: 'Admin',
+        lastName: 'Nimda',
         userSkill: [
             {skill: '542c2b97bac0595474108b48', rating: 2},
             {skill: '542c2b97bac0595474108b49', rating: 1},
@@ -11,7 +13,44 @@ module.exports = [
     {
         email: 'user@ixds.com',
         password: '$2a$10$cIxLgLqYEVfOQl7VQfulL.zzmiTDn3Ugf1vUdu/OHiwQOyvQ.jz6a', // root
+        firstName: 'User',
+        lastName: 'Resu',
         userSkill: [
             {skill: '542c2b97bac0595474108b48', rating: 2}],
         role: 'user'
-    }];
+    },
+    {
+        email: 'alex@ixds.com',
+        password: '$2a$10$cIxLgLqYEVfOQl7VQfulL.zzmiTDn3Ugf1vUdu/OHiwQOyvQ.jz6a', // root
+        firstName: 'Alex',
+        lastName: 'Mirkova',
+        userSkill: [
+            {skill: '542c2b97bac0595474108b50', rating: 1}],
+        role: 'user'
+    },
+    {
+        email: 'john@ixds.com',
+        password: '$2a$10$cIxLgLqYEVfOQl7VQfulL.zzmiTDn3Ugf1vUdu/OHiwQOyvQ.jz6a', // root
+        firstName: 'John',
+        lastName: 'Johanson',
+        userSkill: [
+            {skill: '542c2b97bac0595474108b49', rating: 1},
+            {skill: '542c2b97bac0595474108b53', rating: 1},
+            {skill: '542c2b97bac0595474108b54', rating: 2},
+            {skill: '542c2b97bac0595474108b55', rating: 1}],
+        role: 'user'
+    },
+    {
+        email: 'miriam@ixds.com',
+        password: '$2a$10$cIxLgLqYEVfOQl7VQfulL.zzmiTDn3Ugf1vUdu/OHiwQOyvQ.jz6a', // root
+        firstName: 'Miriam',
+        lastName: 'Müller',
+        userSkill: [
+            {skill: '542c2b97bac0595474108b50', rating: 1},
+            {skill: '542c2b97bac0595474108b48', rating: 0},
+            {skill: '542c2b97bac0595474108b52', rating: 0},
+            {skill: '542c2b97bac0595474108b51', rating: 2}
+            ],
+        role: 'user'
+    }
+];
