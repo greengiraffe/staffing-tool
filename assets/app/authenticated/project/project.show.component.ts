ddb37079d31209679bd3e9e7cfef6f9fa0883670
Adrian Frank <adri13@live.de>
2016-12-18 15:25:19 +0100

    [#82] feat (project/show) -> show project tasks and required skills on detail page

--- assets/app/authenticated/project/project.show.component.ts
+++ assets/app/authenticated/project/project.show.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
 import { ProjectService } from '../../_services/project.service';
+import { SkillService } from '../../_services/skill.service';
 
 import { Project } from "../../_models/project.model";
+import { Skill } from "../../_models/skill.model";
 
 import 'rxjs/add/operator/switchMap';
 
 @Component({
     selector: 'app-project-show',
     templateUrl: 'project.show.template.html',
-    providers: [ ProjectService ]
+    providers: [ ProjectService, SkillService ]
 })
 
 export class ProjectShowComponent implements OnInit {
@@ -18,6 +20,7 @@ export class ProjectShowComponent implements OnInit {
 
     constructor(
         private projectService: ProjectService,
+        private skillService: SkillService,
         private route: ActivatedRoute,
         private router: Router
     ) {}
@@ -26,6 +29,20 @@ export class ProjectShowComponent implements OnInit {
         this.route.params
             .switchMap((params: Params) =>
                 this.projectService.getProjectById(params['id']))
-                .subscribe((project: Project) => this.project = project);
+                .subscribe((project: Project) => {
+                    this.project = project;
+                    if (project.projectTasks)
+                        this.convertSkillIdsToNames();
+
+                });
+    }
+
+    convertSkillIdsToNames() {
+        for (let task of this.project.projectTasks ) {
+            for (let i in task.requiredSkills) {
+                this.skillService.getSkillById(task.requiredSkills[i])
+                    .subscribe((skill: Skill) => task.requiredSkills[i] = skill.name)
+            }
+        }
     }
 }
