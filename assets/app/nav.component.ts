5a808227a16751536ecdcdd16a27bb06d355c294
Marjana Karzek <Marjana Karzek>
2016-12-19 14:20:46 +0100

    [#93] feat(assets/app) -> add nav template and add links

--- assets/app/nav.component.ts
+++ assets/app/nav.component.ts
@@ -3,17 +3,7 @@ import { AuthService } from './_services/auth.service';
 
 @Component({
     selector: 'app-nav',
-    template: `
-      <nav class="navbar navbar-fixed-top navbar-dark bg-inverse">
-        <a routerLink="/home" class="navbar-brand">
-          <i class="fa fa-home" aria-hidden="true"></i>
-           IXDS Staffingtool
-        </a>
-        <ul class="nav navbar-nav">
-          <li class="nav-item"><a class="nav-link" href='#' (click)="auth.logout()">Logout</a></li>
-        </ul>
-      </nav>
-    `,
+    templateUrl: 'nav.template.html'
 })
 
 export class NavComponent {
