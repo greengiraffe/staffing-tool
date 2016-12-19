d1484459c9ef07bc2686f86f5806360d1bc49305
Marjana Karzek <Marjana Karzek>
2016-12-19 14:38:10 +0100

    [#93] refactor(assets/app) -> change dashboard to firtname

--- assets/app/nav.component.ts
+++ assets/app/nav.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from './_services/auth.service';
+import { User } from "./_models/user.model";
+import { UserService } from "./_services/user.service";
+
 
 @Component({
     selector: 'app-nav',
-    templateUrl: 'nav.template.html'
+    templateUrl: 'nav.template.html',
+    providers: [UserService]
 })
 
 export class NavComponent {
+    user: User;
 
-    constructor(private auth: AuthService) {}
+    constructor(private auth: AuthService, private userService: UserService) {}
+
+    ngOnInit() {
+        let currentUserId = localStorage.getItem("userId")
+        this.userService.getUserById(currentUserId)
+            .subscribe(
+                (user: User) => this.user = user,
+                error => console.log(error)
+        );
+    }
 }
