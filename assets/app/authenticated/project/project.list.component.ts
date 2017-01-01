ff85b9417f253965a72dc94a50fb4e79318eb05b
Marjana Karzek <Marjana Karzek>
2017-01-01 22:27:21 +0100

    [#95] feat (assets/app/authenticated/project)-> add delte modal for removing a project

--- assets/app/authenticated/project/project.list.component.ts
+++ assets/app/authenticated/project/project.list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import { ProjectService } from "../../_services/project.service";
 import { Router } from "@angular/router";
 import { Project } from "../../_models/project.model";
+import { ModalService } from "../../_services/modal.service";
 
 @Component({
     selector: 'app-project-list',
@@ -10,17 +11,24 @@ import { Project } from "../../_models/project.model";
 })
 
 export class ProjectListComponent implements OnInit {
+    deleteProjectModalIds = new Array<string>();
     projects: Project[];
     userRole: string;
     userId: string;
 
-    constructor(private projectService: ProjectService, private router: Router) {}
+    constructor(private projectService: ProjectService,
+                private router: Router,
+                private modalService: ModalService) {}
 
     ngOnInit() {
         this.projectService.getProjects()
             .subscribe(
                 (projects: Project[]) => {
                     this.projects = projects;
+                    this.projects.forEach((project, index) => {
+                        this.deleteProjectModalIds.push("deleteProjectModalId" + index);
+                        console.log(this.deleteProjectModalIds[index]);
+                    });
                 }
             );
 
@@ -40,13 +48,13 @@ export class ProjectListComponent implements OnInit {
         this.router.navigate(['/usr/project/show', project.projectId]);
     }
 
-    deleteProject(project: Project) {
-        var r = confirm("Do you want to delete this project?");
-        if(r == true) {
-            this.projectService.deleteProject(project)
-                .subscribe(
-                  result => console.log(result)
-                );
-        }
+    deleteProject(project: Project, index: number) {
+        //var r = confirm("Do you want to delete this project?");
+        //if(r == true) {
+        this.deleteProjectModalIds.splice(index,1);
+        this.projectService.deleteProject(project)
+            .subscribe(
+                result => console.log(result)
+            );
     }
 }
