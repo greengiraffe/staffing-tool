eeb7b2f39ef797fc40408b9ee1fe9388161109f6
Marjana Karzek <Marjana Karzek>
2017-01-02 12:22:13 +0100

    [#95] feat (assets/app/admin/skill) -> add modal to delete button

--- assets/app/admin/skill/skill.list.component.ts
+++ assets/app/admin/skill/skill.list.component.ts
@@ -3,6 +3,7 @@ import { FlashMessagesService } from "angular2-flash-messages";
 
 import { Skill } from "../../_models/skill.model";
 import { SkillService } from "../../_services/skill.service";
+import { ModalService } from "../../_services/modal.service";
 
 
 @Component({
@@ -11,24 +12,35 @@ import { SkillService } from "../../_services/skill.service";
 })
 export class SkillListComponent implements OnInit {
     skills: Skill[];
+    deleteSkillModalIds = new Array<string>();
+    userRole: string;
 
     constructor(private skillService: SkillService,
-    private _flash: FlashMessagesService) {}
+                private _flash: FlashMessagesService,
+                private modalService: ModalService) {}
 
     ngOnInit() {
         this.skillService.getSkills()
             .subscribe(
                 (skills: Skill[]) => {
                     this.skills = skills;
+                    this.skills.forEach((skill, index) => {
+                        this.deleteSkillModalIds.push("deleteSkillModalId" + index);
+                        console.log(this.deleteSkillModalIds[index]);
+                    });
                 }
             );
+        this.userRole = localStorage.getItem('role');
     }
 
-    deleteSkill(skill: Skill) {
+    deleteSkill(skill: Skill, index: number) {
         this.skillService.deleteSkill(skill)
             .subscribe(
-              // data => {this._flash.show("Skill successfully deleted", { cssClass: 'alert-success', timeout: 5000 });},
-              // error => {this._flash.show(error.error.message, { cssClass: 'alert-danger', timeout: 5000 });}
+              data => {
+                  this._flash.show("Skill successfully deleted", { cssClass: 'alert-success', timeout: 5000 });
+                  this.deleteSkillModalIds.splice(index,1);
+              },
+              error => {this._flash.show(error.error.message, { cssClass: 'alert-danger', timeout: 5000 });}
             );
     }
 
