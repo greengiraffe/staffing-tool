9a596a0ab628d2882d1a6afdb0e6676bc3a6c66e
oezert <oezert@tmbp.local>
2017-01-02 11:43:37 +0100

    [#100] feat (project-user-mapping) -> update Project Model For Project Task

--- assets/app/_models/project-task.model.ts
+++ assets/app/_models/project-task.model.ts
@@ -1,8 +1,11 @@
 import { Skill } from "./skill.model";
+import { User } from "./user.model";
 
 export class ProjectTask {
 
     constructor(public title: string,
                 public description: string,
-                public requiredSkills: Skill[]) { }
+                public requiredSkills: Skill[],
+                public taskMembers: User[]
+              ) { }
 }
