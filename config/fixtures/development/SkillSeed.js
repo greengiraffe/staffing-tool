1fc1dbe055c6652a1608bf14846163c6dc2c6863
Valentin von Guttenberg <greengiraffe@posteo.de>
2017-01-03 00:32:04 +0100

    [#00] feat (fixtures) -> extend db seeds and add ProjectSeed

--- config/fixtures/development/SkillSeed.js
+++ config/fixtures/development/SkillSeed.js
@@ -1,15 +1,15 @@
 module.exports = [
     {
         _id: '542c2b97bac0595474108b48',
-        name: 'Angular2'
+        name: 'Angular 2'
     },
     {
         _id: '542c2b97bac0595474108b49',
-        name: 'nodejs'
+        name: 'NodeJS'
     },
     {
         _id: '542c2b97bac0595474108b50',
-        name: 'UI-Design'
+        name: 'UI Design'
     },
     {
         _id: '542c2b97bac0595474108b51',
@@ -21,7 +21,7 @@ module.exports = [
     },
     {
         _id: '542c2b97bac0595474108b53',
-        name: 'Sass'
+        name: 'SASS'
     },
     {
         _id: '542c2b97bac0595474108b54',
@@ -29,6 +29,90 @@ module.exports = [
     },
     {
         _id: '542c2b97bac0595474108b55',
-        name: 'Healthcare'
-    }
+        name: 'MEAN'
+    },
+    {
+        _id: '542c2b97bac0595474108b56',
+        name: 'Information Architecture'
+    },
+    {
+        _id: '542c2b97bac0595474108b57',
+        name: 'Service Design'
+    },
+    {
+        _id: '542c2b97bac0595474108b58',
+        name: 'C++'
+    },
+    {
+        _id: '542c2b97bac0595474108b59',
+        name: 'Unity'
+    },
+    {
+        _id: '542c2b97bac0595474108b60',
+        name: 'Python'
+    },
+    {
+        _id: '542c2b97bac0595474108b61',
+        name: 'Arduino'
+    },
+    {
+        _id: '542c2b97bac0595474108b62',
+        name: 'Rigging'
+    },
+    {
+        _id: '542c2b97bac0595474108b63',
+        name: 'Interviews'
+    },
+    {
+        _id: '542c2b97bac0595474108b64',
+        name: 'Concept'
+    },
+    {
+        _id: '542c2b97bac0595474108b65',
+        name: 'iOS'
+    },
+    {
+        _id: '542c2b97bac0595474108b66',
+        name: 'Web Design'
+    },
+    {
+        _id: '542c2b97bac0595474108b67',
+        name: 'Illustration'
+    },
+    {
+        _id: '542c2b97bac0595474108b68',
+        name: 'Storyboard'
+    },
+    {
+        _id: '542c2b97bac0595474108b69',
+        name: 'Usability'
+    },
+    {
+        _id: '542c2b97bac0595474108b70',
+        name: 'Marketing'
+    },
+    {
+        _id: '542c2b97bac0595474108b71',
+        name: 'Research'
+    },
+    {
+        _id: '542c2b97bac0595474108b72',
+        name: 'Human-Computer-Interaction'
+    },
+    {
+        _id: '542c2b97bac0595474108b73',
+        name: 'Project Management'
+    },
+    {
+        _id: '542c2b97bac0595474108b74',
+        name: 'Backend'
+    },
+    {
+        _id: '542c2b97bac0595474108b75',
+        name: 'Budgeting'
+    },
+    {
+        _id: '542c2b97bac0595474108b76',
+        name: 'Java'
+    },
 ];
