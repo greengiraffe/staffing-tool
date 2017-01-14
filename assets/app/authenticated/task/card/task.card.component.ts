9252d00127ba31a51da8bc9571c52fdd66cabb9c
Jhomar Besmens <jhomar.besmens@outlook.de>
2017-01-14 23:10:22 +0100

    [#120] feat (app/card) -> add basic task card structure

---
+++ assets/app/authenticated/task/card/task.card.component.ts
@@ -0,0 +1,45 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { ProjectTask } from '../../../_models/project-task.model';
+import { ModalService } from "../../../_services/modal.service";
+import { Router } from "@angular/router";
+import { AuthService } from "../../../_services/auth.service";
+import { Project } from '../../../_models/project.model';
+
+@Component({
+    selector: 'app-task-card',
+    templateUrl: 'task.card.template.html',
+    styleUrls: ['task.card.style.scss']
+})
+
+export class CardComponent {
+
+    @Input ("project") project: Project;
+    @Input("ProjectTask") projectTask: ProjectTask;
+    @Output("onDelete") onDelete = new EventEmitter();
+
+    private currentUserCanDelete = false;
+    private currentUserCanEdit = false;
+    private deleteTaskModalId = "deleteTaskModal" + (0 | Math.random() * 6.04e7).toString(36);
+
+
+    constructor(private modalService: ModalService, private authService: AuthService, private router: Router) {
+    }
+
+    ngOnInit() {
+        const user = this.authService.currentUser();
+
+        if (user) {
+            this.currentUserCanDelete = user.role == "admin" || this.project.creator._id == user._id;
+            this.currentUserCanEdit = user.role == "admin" || this.project.creator._id == user._id;
+        }
+    }
+
+    showTask() {
+        this.router.navigate(['/usr/task/show', this.projectTask]);
+    }
+
+    deleteTask() {
+        this.onDelete.emit(this.projectTask);
+        this.modalService.close(this.deleteTaskModalId)
+    }
+}
