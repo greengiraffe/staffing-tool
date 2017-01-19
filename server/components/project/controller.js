0d5dbee67a913f45227e652a656d11dcee7af973
oezert <oezert@tmbp.fritz.box>
2017-01-19 03:16:37 +0100

    [#108] fix (project-routes) -> add check to validate id in project routes

--- server/components/project/controller.js
+++ server/components/project/controller.js
@@ -3,7 +3,13 @@ let router = express.Router();
 let Project = require('./project');
 let mongoose = require('mongoose');
 
-//create Projekt
+function isValidID(id) {
+    if(!mongoose.Types.ObjectId.isValid(id)) {
+        res.status(400).json({message: "Invalid ID"});
+        return false;
+    }
+    return true;
+}
 
 /**
  * Create Project
@@ -35,52 +41,64 @@ router.get('/project/list', function(req, res, next) {
  * Get specific project by id
  */
 router.get('/project/:id', function(req, res, next) {
-    Project.getProjectById(req.params.id)
-        .then(function (result) {
-            res.status(200).json(result);
-        })
-        .catch(function (err) {
-            res.status(400).json(err);
-        })
+    let id = req.params.id;
+    if(isValidID(id)) {
+        Project.getProjectById(req.params.id)
+            .then(function (result) {
+                res.status(200).json(result);
+            })
+            .catch(function (err) {
+                res.status(400).json(err);
+            })
+    }
 });
 
 /**
  * Add Task to Project
  */
 router.post('/project/task/:id', function(req, res, next) {
-    Project.createProjectTask(req.params.id, req.body)
-        .then(function(result){
-           res.status(201).json(result);
-        })
-        .catch(function(err){
-            res.status(500).json(err);
-        });
+    let id = req.params.id;
+    if(isValidID(id)) {
+        Project.createProjectTask(req.params.id, req.body)
+            .then(function(result){
+               res.status(201).json(result);
+            })
+            .catch(function(err){
+                res.status(500).json(err);
+            });
+    }
 });
 
 /**
  * Remove Task from Project
  */
 router.delete('/project/task/:id', function(req, res, next) {
-    Project.removeProjectTask(req.params.id, req.body)
-        .then(function(result){
-           res.status(200).json(result);
-        })
-        .catch(function(err){
-            res.status(err.statusCode).json(err);
-        });
+    let id = req.params.id;
+    if(isValidID(id)) {
+        Project.removeProjectTask(req.params.id, req.body)
+            .then(function(result){
+               res.status(200).json(result);
+            })
+            .catch(function(err){
+                res.status(err.statusCode).json(err);
+            });
+    }
 });
 
 /**
  * Update a Task
  */
 router.put('/project/task/:id', function(req, res, next) {
-    Project.updateProjectTask(req.params.id, req.body)
-        .then(function(result){
-           res.status(200).json(result);
-        })
-        .catch(function(err){
-            res.status(err.statusCode).json(err);
-        });
+    let id = req.params.id;
+    if(isValidID(id)) {
+        Project.updateProjectTask(req.params.id, req.body)
+            .then(function(result){
+               res.status(200).json(result);
+            })
+            .catch(function(err){
+                res.status(err.statusCode).json(err);
+            });
+    }
 });
 
 /**
@@ -101,17 +119,13 @@ router.put('/project', function(req, res, next) {
  */
 router.delete('/project/:id', function(req, res, next) {
     let id = req.params.id;
-    if(mongoose.Types.ObjectId.isValid(id)) {
+    if(isValidID(id)) {
         Project.removeProject(id)
         .then(function(result) {
             res.status(200).json(result);
         })
         .catch(function(err) {
             res.status(err.statusCode).json(err);
-        });
-    } else {
-        res.status(400).json({
-            message: "Invalid ID"
         });
     }
 });
