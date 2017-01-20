a0c0438ac88d240b78c1e9b92a47f14a078b9be6
oezert <oezert@tmbp.local>
2017-01-20 12:33:20 +0100

    [#132] feat (auth) -> setup bearer strategy for passport

--- server/services/passport.js
+++ server/services/passport.js
@@ -1,61 +1,25 @@
 let passport = require('passport');
-let LocalStrategy = require('passport-local').Strategy;
-let JwtStrategy = require('passport-jwt').Strategy;
-let extractJwt = require('passport-jwt').ExtractJwt;
-
-let authHelper = require('./authHelper');
-let User = require('../components/user/user');
+var bearerStrategy = require('passport-http-bearer').Strategy;
+let jwt = require('jsonwebtoken');
+let config = require('../../config/config');
 
 let setupPassport = function () {
 
-    passport.serializeUser((user, done) => {
-        done(null, user.id);
-    });
+    passport.use('bearer', new bearerStrategy( function(token, cb) {
+        jwt.verify(token, config.jwt.secret, function(err, decoded) {
+            if (err) {
+                return cb(err);
+            }
 
-    passport.deserializeUser((mail, done) => {
-        User.getUserByMail(mail)
-            .then((user) => {
-                done(null, user);
-            })
-            .catch((err) => {
-                done(err, null);
-            });
-    });
+            let user = decoded.user
+            if (!user) {
+                return cb(null, false);
+            }
 
-    passport.use('local', new LocalStrategy({
-        usernameField: 'email',
-        passwordField: 'password'
-    }, function (email, password, done) {
-        User.getUserByMail(email)
-            .then((user) => {
-
-                if (!user) {
-                    return done(null, false,
-                        req.flash('message', 'User Not found.'));
-                }
-
-                if (authHelper.comparePassword(password, user.password)) {
-                    return done(null, user);
-                } else {
-                    return done(null, false,
-                        req.flash('message', 'Invalid Password'));
-                }
-            })
-            .catch((err) => {
-                return done(err);
-            });
+            return cb(null, user);
+        });
     }));
 
-    passport.use('jwt', new JwtStrategy({
-        jwtFromRequest: extractJwt.fromAuthHeader(),
-        secretOrKey: 'abc',
-    }, function(payload, done) {
-        User.getUserByID(payload.sub)
-            .then((user) => {
-                done(null, user);
-            })
-            .catch(err => done(err, false));
-    }))
 };
 
 module.exports = setupPassport;
