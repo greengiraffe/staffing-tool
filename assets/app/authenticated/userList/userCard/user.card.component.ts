0c48ba3336a6c2d034c673bbb947b6ddb3463543
Duc Anh Phi <ducanhphi@gmail.com>
2017-01-24 16:06:34 +0100

    [#142] feat (userList) -> add missing userList component

---
+++ assets/app/authenticated/userList/userCard/user.card.component.ts
@@ -0,0 +1,35 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { ModalService } from "../../../_services/modal.service";
+import { Router } from "@angular/router";
+import { AuthService } from "../../../_services/auth.service";
+import { User } from "../../../_models/user.model";
+
+@Component({
+    selector: 'app-user-card',
+    templateUrl: 'user.card.template.html',
+    styleUrls: ['user.card.style.scss']
+})
+
+export class UserCardComponent {
+
+    @Input("user") user: User;
+    @Output("onDelete") onDelete = new EventEmitter();
+
+    private currentUserCanDelete = false;
+    private deleteUserModalId = "deleteUserModal" + (0|Math.random()*6.04e7).toString(36);
+
+    constructor(private modalService: ModalService, private authService: AuthService, private router: Router) {}
+
+    ngOnInit() {
+        const user = this.authService.currentUser();
+
+        if (user) {
+            this.currentUserCanDelete = user.role === "admin";
+        }
+    }
+
+    deleteUser() {
+        this.onDelete.emit(this.user);
+        this.modalService.close(this.deleteUserModalId)
+    }
+}
