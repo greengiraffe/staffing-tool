65d107a95f373e6668b77ec57a2f1a12bb5293c6
Jhomar Besmens <jhomar.besmens@outlook.de>
2017-01-25 16:14:08 +0100

    [#141] fix (user.search) -> reduce fractional digits

--- assets/app/authenticated/userSearch/user.search.component.ts
+++ assets/app/authenticated/userSearch/user.search.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import {Component, OnInit, Input, Renderer} from '@angular/core';
 import { User } from '../../_models/user.model';
 import { Skill } from "../../_models/skill.model";
 import { UserService } from '../../_services/user.service';
@@ -16,14 +16,14 @@ import { UserSearchService } from '../../_services/user.search.service';
         <div class="user" *ngFor="let interested of interestedUsers | filterNames : 'firstName' : 'lastName' : searchText"
          (click)="selectUser(user)">
          <div class="user-info">
-            {{ interested.firstName }} {{ interested.lastName }}
+            <img class="avatar {{user._id}}" alt="Profile picture">{{ interested.firstName }} {{ interested.lastName }}
             <span class="tag tag-default">interested</span>
          </div>
          <div class="user-match" *ngIf="interested.match || interested.match === 0">{{ interested.match | percent}}</div>
         </div>
         <div class="user" *ngFor="let user of visibleUsers | filterNames : 'firstName' : 'lastName' : searchText" (click)="selectUser(user)">
-            <div class="user-info">{{ user.firstName }} {{ user.lastName }}</div>
-            <div class="user-match" *ngIf="user.match || user.match === 0">{{ user.match | percent}}</div>
+            <div class="user-info"><img class="avatar {{user._id}}" alt="Profile picture">{{ user.firstName }} {{ user.lastName }}</div>
+            <div class="user-match" *ngIf="user.match || user.match === 0">{{ user.match | percent }}</div>
         </div>
       </div>
 
@@ -40,9 +40,12 @@ export class UserSearchComponent implements OnInit {
     hiddenUsers: any[] = [];
     searchText;
     showUserList = false;
+    urlsOfLoadedPictures: Object = new Object();
+    idsOfLoadedPictures = new Array<string>();
 
     constructor(private userService: UserService,
-                private userSearchService: UserSearchService){}
+                private userSearchService: UserSearchService,
+                private renderer: Renderer){}
 
     ngOnInit(){
         this.userService.getUsers()
@@ -126,7 +129,7 @@ export class UserSearchComponent implements OnInit {
                         }
                     }
                 }
-                user.match = (user.match / requiredSkills.length);
+                user.match = (user.match / requiredSkills.length).toFixed(3);
             }
         }
         doCalculation(this.visibleUsers);
@@ -141,5 +144,30 @@ export class UserSearchComponent implements OnInit {
         this.calculateMatch(this.requiredSkills);
         this.sortUsers();
         this.showUserList = true;
+    }
+
+    loadUserAvatars(users: Array<User>) {
+
+            users.forEach(user => {
+                if(!this.urlsOfLoadedPictures[user._id]) {
+                    this.userService.getUserImage(user._id, "small")
+                        .subscribe(data => {
+                                this.urlsOfLoadedPictures[user._id] = data;
+                                this.renderImages(user._id)
+                            },
+                            error => {
+                                this.urlsOfLoadedPictures[user._id] = '/img/usersmall.png';
+                                this.renderImages(user._id)
+                            });
+                }
+            });
+
+    }
+
+    renderImages(userId: string) {
+        var images = document.getElementsByClassName(userId);
+        for (var i = 0; i < images.length; ++i) {
+            this.renderer.setElementProperty(images[i], 'src', this.urlsOfLoadedPictures[userId])
+        }
     }
 }
