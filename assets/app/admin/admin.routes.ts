9381f9c8b773cc3089409802a353ed6be234e171
oezert <oezert@tmbp.fritz.box>
2017-01-29 18:44:55 +0100

    [#132] feat (auth) -> setup guard for admin routes

--- assets/app/admin/admin.routes.ts
+++ assets/app/admin/admin.routes.ts
@@ -3,6 +3,7 @@ import { NgModule } from "@angular/core";
 
 import { UserCreateComponent } from "./userCreate/user.create.component";
 import { SkillListComponent } from "./skillList/skill.list.component";
+import { CanActivateAuthGuard } from '../app.routing.guard.auth';
 
 const adminRoutes: Routes = [
     {
@@ -11,8 +12,8 @@ const adminRoutes: Routes = [
             {
                 path: '',
                 children: [
-                    { path: 'user/create', component: UserCreateComponent },
-                    { path: 'skills', component: SkillListComponent }
+                    { path: 'user/create', component: UserCreateComponent, canActivate: [CanActivateAuthGuard]},
+                    { path: 'skills', component: SkillListComponent, canActivate: [CanActivateAuthGuard]}
                 ]
             }
         ]
