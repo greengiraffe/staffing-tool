2e21dbf0be6f19084bf98fed63007ba3ac50acc0
oezert <oezert@tmbp.fritz.box>
2017-01-29 02:36:02 +0100

    [#132] fix (auth) -> add redirect to routing guard

--- assets/app/app.routing.guard.auth.ts
+++ assets/app/app.routing.guard.auth.ts
@@ -1,14 +1,18 @@
 import { Injectable } from '@angular/core';
 import { CanActivate } from '@angular/router';
 import { AuthService } from './_services/auth.service';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class CanActivateAuthGuard implements CanActivate {
 
-    constructor(private authService: AuthService) {}
+    constructor(private authService: AuthService, private router: Router) {}
 
     canActivate() {
-        return this.authService.loggedIn();
+        if (!this.authService.loggedIn()) {
+            this.router.navigate(['']);
+            return false;
+        }
+        return true;
     }
 }
-// TODO
