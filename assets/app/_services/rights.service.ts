034e3bf690dd48fa196628a4161684092be617ba
Valentin von Guttenberg <greengiraffe@posteo.de>
2017-01-30 13:19:22 +0100

    [#155] feat (rights) -> prevent assigned users from editing the project

--- assets/app/_services/rights.service.ts
+++ assets/app/_services/rights.service.ts
@@ -6,7 +6,11 @@ import { User } from "../_models/user.model";
 export class RightsService {
 
     canEditTask(project: Project, user: User): boolean {
-        return user.role === "admin" || project.creator._id === user._id;
+        switch(user.role) {
+            case "freelancer":
+                return false;
+        }
+        return user.role === "admin" || project.creator._id === user._id || this.isAssignedUser(user, project);
     }
 
     canDeleteTask(project: Project, user: User): boolean {
@@ -18,31 +22,26 @@ export class RightsService {
     }
 
     canEditProject(project: Project, user: User): boolean {
-        if(user.role === 'freelancer'){
-            return false;
-        }
-
-        let isAssignedUser = user => {
-            for(let task of project.projectTasks) {
-                for(let assignedUser of task.assignedUsers) {
-                    if(assignedUser._id === user._id) {
-                        return true;
-                    }
-                }
-            }
-        };
-
-        return user.role === 'admin' ||
-            project.creator._id === user._id ||
-            isAssignedUser(user);
+        return user.role === 'admin' || project.creator._id === user._id;
     }
 
     canRemoveSkill(user: User): boolean {
-        return user.role === 'admin' || user.role === 'user_creator'
+        return user.role === 'admin' || user.role === 'user_creator';
     }
 
     canRemoveUser(user: User): boolean {
-        return user.role === 'admin' || user.role === 'user_creator'
+        return user.role === 'admin' || user.role === 'user_creator';
     }
+
+    private isAssignedUser(user: User, project: Project): boolean {
+        for(let task of project.projectTasks) {
+            for(let assignedUser of task.assignedUsers) {
+                if(assignedUser._id === user._id) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    };
 
 }
